4ae9deb9bd00d4d3b7024beda57af25436ce947d
Erinaldo Rodrigues <erinaldors@gmail.com>
2026-07-10 00:05:59 -0300

    feat(infra): create infrastructure foundation

---
+++ scripts/services/portainer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source "$(dirname "$0")/../common/init.sh"
+
+dcp_title "Portainer"
+
+cd "$HOME/infrastructure/compose/portainer"
+
+docker compose up -d
+
+dcp_success "Portainer iniciado."
